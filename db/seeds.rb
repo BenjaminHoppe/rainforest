97e1e832d976068ed1dcb50940c86c31aea31c4e
Benjamin <hoppeconnect@gmail.com>
2018-12-11 10:53:37 -0500

    added associations and tested it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Review.delete_all
+prodcut_1 = Product.create!(name: "cup", description: "thing you drink out of", price: 1000000.01)
+
+review_1 = Review.create!(body: "overpriced, would only buy if on sale", product_id: product_1.id)
