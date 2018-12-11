5a66c828d88940e0d1c5fe117346b698f2ff5483
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-12-11 11:26:51 -0500

    created products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def show
+        @product = Product.find(params[:id])
+
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new
+
+        @product.name = params[:product][:name]
+        @product.description = params[:product][:description]
+        @product.price = params[:product][:price]
+
+        if @product.save
+            redirect_to "/products/#{@product.id}"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+
+        @product.name = params[:product][:name]
+        @product.description = params[:product][:description]
+        @product.price = params[:product][:price]
+
+        if @product.save
+            redirect_to "/products/#{@product.id}"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to"/products"
+    end
+
+
+end
