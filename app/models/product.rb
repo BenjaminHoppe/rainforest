195224c3108f106d50469beb5ae9e09a3dd4a59c
Benjamin <hoppeconnect@gmail.com>
2018-12-11 15:52:47 -0500

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews
+  validates :name, :price, :description, presence: true
+  validates :price, numericality: true
+  validates :name, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :products
+  validates :body, presence: true
 end
